1b46739263e9367bf01c5f83b82e60b156910ad2
eugbrh <baranovevge@gmail.com>
2024-12-25 16:52:30 +0300

    completed project

--- app.py
+++ app.py
@@ -1,27 +1,231 @@
-from src import Location
-from flask import Flask, render_template, request
+from src import Location, Weather
 from api_keys import ACCUWEATHER_API_KEY, YANDEX_API_KEY
+from dash import Dash, html, dcc, Input, Output, State, ALL
+import plotly.graph_objects as go
+import pandas as pd
+import logging
+from dash import dash_table
 
-def get_return(data):
-    return f"Температура °C:  {data['temperature']}\nВлажность %:  {data['humidity']}\nСкорость ветра м/с:  {data['wind_speed']}\nВероятность осадков %:  {data['precipitation_prob']}"
+app = Dash(__name__)
 
-app = Flask(__name__)
+location = Location(accuweather_api_key=ACCUWEATHER_API_KEY, yandex_api_key=YANDEX_API_KEY)
+weather = Weather(accuweather_api_key=ACCUWEATHER_API_KEY)
 
-point = Location(ACCUWEATHER_API_KEY, YANDEX_API_KEY)
+app.layout = html.Div(
+    [
+        html.H1("Прогноз погоды на маршруте"),
+        html.Div(
+            [
+                html.Label("Введите города на маршруте:"),
+                html.Div(
+                    [
+                        dcc.Input(
+                            id={"type": "city-input", "index": 0},
+                            type="text",
+                            placeholder="Город 1",
+                        ),
+                    ],
+                    id="city-input-container",
+                ),
+                html.Button("Добавить город в маршрут", id="add-city", n_clicks=0),
+            ]
+        ),
+        html.Div(
+            [
+                html.Label("На сколько дней показать прогноз?"),
+                dcc.Dropdown(
+                    id="forecast-days",
+                    options=[
+                        {"label": "1 день", "value": 1},
+                        {"label": "2 дня", "value": 2},
+                        {"label": "3 дня", "value": 3},
+                        {"label": "4 дня", "value": 4},
+                        {"label": "5 дней", "value": 5},
+                    ],
+                    value=1,
+                ),
+            ]
+        ),
+        html.Button("Получить прогноз", id="submit-button", n_clicks=0),
+        html.Div(id="output-container"),
+    ]
+)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    result = None
-    if request.method == "POST":
-        start_city = request.form.get("start_city")
-        end_city = request.form.get("end_city")
+@app.callback(Output("city-input-container", "children"), Input("add-city", "n_clicks"), State("city-input-container", "children"))
+def add_city(n_clicks, children):
+    try:
+        new_children = children.copy()
+        new_input = dcc.Input(
+            id={"type": "city-input", "index": n_clicks},
+            type="text",
+            placeholder=f"Город {n_clicks + 2}",
+        )
+        new_children.append(new_input)
+        return new_children
+    except Exception as e:
+        logging.error(f"Ошибка при добавлении города: {str(e)}")
+        return children
 
-        start_weather_comment, start_weather_data = point.get_weather(start_city)
-        end_weather_comment, end_weather_data = point.get_weather(end_city)
 
-        result = f"Погода в городе {start_city}:\n\n{get_return(start_weather_data)}\n\nОценка: {start_weather_comment}\n\nПогода в городе {end_city}:\n\n{get_return(end_weather_data)}\n\nОценка: {end_weather_comment}"
+@app.callback(
+    Output("output-container", "children"),
+    Input("submit-button", "n_clicks"),
+    State({"type": "city-input", "index": ALL}, "value"),
+    State("forecast-days", "value")
+)
+def update_output(n_clicks, cities, forecast_days):
+    if n_clicks > 0:
+        all_cities = [city for city in cities if city]
+        if not all_cities:
+            return html.Div("Должен быть введен хотя бы один город")
+        if any(any(char.isdigit() for char in element) for element in all_cities):
+            return html.Div("В названии городов не должны присутстовать цифры")
 
-    return render_template("index.html", result=result)
+        weather_data = {}
+        city_coordinates = []
+        errors = []
+        for city in all_cities:
+            try:
+                lat, lon = location.get_coordinates(city)
+                location_key = location.get_location_key(lat, lon)
+                if not location_key:
+                    errors.append(f"Не удалось найти город <<{city}>>")
+                    continue
+                forecast = weather.get_forecast_data(location_key, days=forecast_days)
+                if not forecast:
+                    errors.append(f"Не удалось получить прогноз для города <<{city}>>")
+                    continue
+                weather_data[city] = {
+                    "forecast": forecast,
+                    "latitude": lat,
+                    "longitude": lon,
+                }
+                city_coordinates.append({"city": city, "lat": lat, "lon": lon})
+            except Exception as e:
+                errors.append(f"Ошибка обработки города <<{city}>>: {str(e)}")
+
+        if not weather_data:
+            return html.Div(errors, style={"color": "red"})
+
+        output_children = []
+
+        if errors:
+            output_children.append(html.Div(errors))
+
+        try:
+            df_city_coordinates = pd.DataFrame(city_coordinates)
+            fig = go.Figure()
+
+            fig.add_trace(
+                go.Scattermapbox(
+                    lat=df_city_coordinates["lat"],
+                    lon=df_city_coordinates["lon"],
+                    mode="markers+lines",
+                    marker=go.scattermapbox.Marker(size=9),
+                    text=df_city_coordinates["city"],
+                )
+            )
+
+            fig.update_layout(
+                mapbox_style="open-street-map",
+                mapbox_zoom=3,
+                mapbox_center_lat=df_city_coordinates["lat"].mean(),
+                mapbox_center_lon=df_city_coordinates["lon"].mean(),
+                height=500,
+            )
+
+            fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+            output_children.append(dcc.Graph(figure=fig))
+
+            for city, data in weather_data.items():
+                forecast = data["forecast"]
+                dates = []
+                min_temps = []
+                max_temps = []
+                wind_speeds = []
+                precipitation_probs = []
+                for cnt, day in enumerate(forecast["DailyForecasts"]):
+                    if (cnt >= forecast_days):
+                        break
+                    dates.append(day["Date"][:10])
+                    min_temps.append(day["Temperature"]["Minimum"]["Value"])
+                    max_temps.append(day["Temperature"]["Maximum"]["Value"])
+                    wind_speeds.append(day["Day"]["Wind"]["Speed"]["Value"])
+                    precipitation_probs.append(day["Day"]["PrecipitationProbability"])
+
+                temp_trace_min = go.Scatter(x=dates, y=min_temps, mode="lines+markers", name="Мин. температура")
+                temp_trace_max = go.Scatter(x=dates, y=max_temps, mode="lines+markers", name="Макс. температура")
+
+                temp_layout = go.Layout(
+                    title=f"Температура в городе '{city}'",
+                    yaxis={"title": "Температура (°C)"},
+                )
+                temp_fig = go.Figure(
+                    data=[temp_trace_min, temp_trace_max], layout=temp_layout
+                )
+                output_children.append(dcc.Graph(figure=temp_fig))
+
+                wind_trace = go.Bar(x=dates, y=wind_speeds, name="Скорость ветра")
+                wind_layout = go.Layout(
+                    title=f"Скорость ветра в городе '{city}'",
+                    yaxis={"title": "Скорость ветра (км/ч)"},
+                )
+                wind_fig = go.Figure(data=[wind_trace], layout=wind_layout)
+                output_children.append(dcc.Graph(figure=wind_fig))
+
+                precipitation_prob_trace = go.Bar(x=dates, y=precipitation_probs, name="Вероятность осадков")
+                precipitation_prob_layout = go.Layout(
+                    title=f"Вероятность осадков в городе '{city}'",
+                    yaxis={"title": "Вероятность (%)"},
+                )
+                precip_fig = go.Figure(data=[precipitation_prob_trace], layout=precipitation_prob_layout)
+                output_children.append(dcc.Graph(figure=precip_fig))
+
+            table_data = []
+            for city, data in weather_data.items():
+                forecast = data["forecast"]
+                for cnt, day in enumerate(forecast["DailyForecasts"]):
+                    if (cnt >= forecast_days):
+                        break
+                    table_data.append({
+                        "Город": city,
+                        "Дата": day["Date"][:10],
+                        "Мин. температура (°C)": day["Temperature"]["Minimum"]["Value"],
+                        "Макс. температура (°C)": day["Temperature"]["Maximum"]["Value"],
+                        "Скорость ветра (км/ч)": day["Day"]["Wind"]["Speed"]["Value"],
+                        "Вероятность осадков (%)": day["Day"]["PrecipitationProbability"]
+                    })
+
+            table = dash_table.DataTable(
+                id='weather-table',
+                columns=[
+                    {"name": "Город", "id": "Город"},
+                    {"name": "Дата", "id": "Дата"},
+                    {"name": "Мин. температура (°C)", "id": "Мин. температура (°C)"},
+                    {"name": "Макс. температура (°C)", "id": "Макс. температура (°C)"},
+                    {"name": "Скорость ветра (км/ч)", "id": "Скорость ветра (км/ч)"},
+                    {"name": "Вероятность осадков (%)", "id": "Вероятность осадков (%)"},
+                ],
+                data=table_data,
+                style_table={'height': '400px', 'overflowY': 'auto'},
+                style_cell={'textAlign': 'center', 'padding': '5px'},
+                style_header={'fontWeight': 'bold', 'textAlign': 'center'},
+            )
+
+            output_children.append(table)
+
+        except Exception as e:
+            print(f'Ошибка: {str(e)}')
+            logging.error(f"Ошибка при создании графиков: {str(e)}")
+            return html.Div(f"Произошла ошибка: {str(e)}. Пожалуйста, попробуйте еще раз.", style={"color": "red"})
+
+        return output_children
+    else:
+        return html.Div()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    try:
+        app.run_server(debug=True)
+    except Exception as e:
+        logging.error(f"Ошибка при запуске сервера: {str(e)}")
--- src.py
+++ src.py
@@ -1,132 +1,116 @@
 import requests
+from api_keys import ACCUWEATHER_API_KEY
 
-class Weather:
-    def __init__(self, loc_key, accu_api_key):
-        self.api_key = accu_api_key
-        self.loc_key = loc_key
-        self.weather = {}
-        self.data = {}
-    
-    def get_current_weather_data(self):
-        '''
-        Получает погодные данные по location key
-        '''
-        params = {
-        'apikey': self.api_key,
-        'language': 'ru',
-        'details': 'true'
-        }
-        response = requests.get(f"http://dataservice.accuweather.com/currentconditions/v1/{self.loc_key}", params=params)
-        data = response.json()
-        if data:
-            self.weather['temperature'] = data[0]['Temperature']['Metric']['Value']
-            self.weather['humidity'] = data[0]['RelativeHumidity']
-            self.weather['wind_speed'] = data[0]['Wind']['Speed']['Metric']['Value']
-            
-            return data
-        else:
-            return None
-        
-    def get_forecast_data(self):
-        params = {
-        'apikey': self.api_key,
-        'language': 'ru',
-        'details': 'true',
-        'metric': 'true'
-        }
-        response = requests.get(f"http://dataservice.accuweather.com/forecasts/v1/daily/1day/{self.loc_key}", params=params)
-        data = response.json()
-        if data:
-            self.weather['precipitation_prob'] = data['DailyForecasts'][0]['Day']['PrecipitationProbability']
-            return data
-        else:
-            return None
-    
-    def get_weather(self):
-        self.get_current_weather_data()
-        self.get_forecast_data()
-        return self.weather
-    
-    def check_bad_weather(self):
-        ''' 
-        Функция для оценки неблагоприятных погодных условий 
-        '''
-        weather = self.weather
-        warnings = []
+find_city = ''
 
-        if weather['temperature'] < 0 or weather['temperature'] > 35:
-            warnings.append("Неприятная температура.")
-        if weather['wind_speed'] > 50:
-            warnings.append("Сильный ветер.")
-        if weather['precipitation_prob'] > 70:
-            warnings.append("Высокая вероятность осадков.")
-
-        if warnings:
-            return "Неблагоприятные погодные условия: " + " ".join(warnings)
-        else:
-            return "Погодные условия благоприятные."
+class APIQuotaExceededError(Exception):
+    pass
 
 class Location:
-    def __init__(self, accu_api_key, ya_api_key):
-        self.accu = accu_api_key
-        self.ya = ya_api_key
-        self.lat = ''
-        self.lon = ''
-    
-    def ya_request(self, city: str):
-        params = {'apikey': self.ya,
-                  'geocode': city,
-                  'lang': 'ru_RU',
-                  'kind': 'locality',
-                  'format': 'json'}
-        
+    def __init__(self, accuweather_api_key, yandex_api_key):
+        self.yandex_key = yandex_api_key
+        self.accuweather_key = accuweather_api_key
+
+    def request_to_yandex(self, city: str):
+        params = {
+            'apikey': self.yandex_key,
+            'geocode': city,
+            'lang': 'ru_RU',
+            'format': 'json'
+        }
+
         response = requests.get('https://geocode-maps.yandex.ru/1.x', params=params)
 
         if response.status_code != 200:
-            print('Ошибка при получении данных:', response.json())
-            return None
-
+            print(f'Ошибка при получении данных. Код ошибки: {response.status_code}')
+            return (f'Ошибка при получении данных. Код ошибки: {response.status_code}')
         return response.json()
-    
-    def get_coords(self, city: str):
-        '''
-        Возвращает координаты города
-        '''
-        data = self.ya_request(city)
 
+
+    def get_coordinates(self, city: str):
+        global find_city
+        data = self.request_to_yandex(city)
         if data:
             try:
                 coords = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos']
+                find_city = data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['name']
                 lon, lat = coords.split(' ')
-                self.lat = lat
-                self.lon = lon
-                return str(lon), str(lat)
-            except KeyError:
-                print('Не удалось получить координаты')
-                return None, None
+                lat = float(lat)
+                # print(f"-------------lat------------")
+                # print(f"------------{city}------------")
+                # print(lat)
+                lon = float(lon)
+                # print(f"-------------lon------------")
+                # print(f"------------{city}------------")
+                # print(lon)
+                return lat, lon
+            except Exception as e:
+                raise Exception(f"Ошибка получения координат: {e}")
         return None, None
-    
-    def get_key(self, city: str):
-        '''
-        Возвращает location key по координатам
-        '''
-        self.get_coords(city)
-        params = {'apikey': self.accu,
-                  'q': f'{self.lat},{self.lon}'}
-        response = requests.get('http://dataservice.accuweather.com/locations/v1/cities/geoposition/search', params = params)
+
+
+    def get_location_key(self, lat, lon):
+        params = {
+            'apikey': self.accuweather_key,
+            'q': f'{lat},{lon}'
+        }
+        response = requests.get('http://dataservice.accuweather.com/locations/v1/cities/geoposition/search', params=params)
+
+        if response.status_code == 503 or 'ServiceUnavailable' in response.json().get('Code', ''):
+            print("APIQuotaExceededError вызывается")
+            raise APIQuotaExceededError("Запросы к API закончились")
 
         if response.status_code != 200 and response.status_code != 201:
             print('Ошибка при получении данных:', response.json())
-            return
-        
+            raise Exception(f'Ошибка при получении данных. Код ошибки: {response.status_code}')
+
         return response.json()['Key']
-    
-    def get_weather(self, city: str):
-        '''
-        Возвращает комментарий о погодных условиях и погодные условия в городе
-        '''
-        key = self.get_key(city)
-        point = Weather(key, self.accu)
-        data = point.get_weather()
-        comment = point.check_bad_weather()
-        return (comment, data)
+
+class Weather:
+    def __init__(self, accuweather_api_key):
+        self.accuweather_key = accuweather_api_key
+        self.weather = {}
+
+    def get_forecast_data(self, location_key, days=5):
+        if days == 1:
+            forecast_url = (
+                f"http://dataservice.accuweather.com/forecasts/v1/daily/1day/{location_key}"
+            )
+        else:
+            forecast_url = (
+                f"http://dataservice.accuweather.com/forecasts/v1/daily/5day/{location_key}"
+            )
+        params = {
+            "apikey": ACCUWEATHER_API_KEY,
+            "language": "ru",
+            "details": "true",
+            "metric": "true",
+        }
+        response = requests.get(forecast_url, params=params)
+        data = response.json()
+        if data:
+            return data
+        else:
+            return
+
+    def check_bad_weather(self):
+        weather = self.weather
+        estimation = []
+
+        if 'temperature' not in weather or 'wind_speed' not in weather or 'precipitation_prob' not in weather:
+            raise KeyError("Недостаточно данных для оценки погодных условий.")
+
+        if weather['temperature'] < 0 or weather['temperature'] > 35:
+            estimation.append("Неприятная температура")
+        if weather['wind_speed'] > 50:
+            estimation.append("Очень сильный ветер")
+        if weather['precipitation_prob'] > 70:
+            estimation.append("Могут быть осадки")
+
+        if estimation:
+            answer = "Плохая погода: \n"
+            for note in estimation:
+                answer = answer + '   - ' + note + '\n'
+            return answer
+        else:
+            return "Погода хорошая"
